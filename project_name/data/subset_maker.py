5d7931b567484ba038de9fb16938e32602c26fb7
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-10 09:29:22 +0200

    made code part for making data sample folders and copying over. still needs to set file endings and normality test

--- project_name/data/subset_maker.py
+++ project_name/data/subset_maker.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 
 
 def get_all_data_pathnames() -> list[list[str]]:
@@ -16,17 +17,20 @@ def get_all_data_pathnames() -> list[list[str]]:
 
     for main_folder in datapoint_directories_main:
         datapoint_directories = [main_folder]
-        for _ in range(2):
+        for _ in range(3):
             new_datapoint_directories = []
 
             for path in datapoint_directories:
                 sub_folders = os.listdir(path)
 
                 for folder in sub_folders:
+                    # TODO get rid of file extension and type description.
                     new_datapoint_directories.append(os.path.join(path,
                                                                   folder))
 
-            datapoint_directories = new_datapoint_directories
+            # get rid of all duplicates
+            datapoint_directories = list(set(new_datapoint_directories))
+
         all_datapoint_folder_pathnames.append(datapoint_directories)
 
     return all_datapoint_folder_pathnames
@@ -41,6 +45,10 @@ def get_train_data_folders() -> list[str]:
 
 
 def subset_full_dataset(amount_samples: int):
+    file_directory = __file__
+    data_directory = os.path.join(os.path.split(file_directory)[0],
+                                  "subset_data")
+
     all_datapoints_folder_pathnames = get_all_data_pathnames()
 
     amount_differnt_main_folder = len(all_datapoints_folder_pathnames)
@@ -49,12 +57,31 @@ def subset_full_dataset(amount_samples: int):
     selected_data: list[list[str]] = [
         random.sample(main_folder, sample_per_main_folder) for
         main_folder in all_datapoints_folder_pathnames]
+    flatten_selected_data = []
+
+    for foldered_data in selected_data:
+        flatten_selected_data += foldered_data
 
     # TODO test normality.
 
+    # TODO list all file endings.
+    file_endings = []
+    # copy over the subsetted data in their own folders.
+    for index, data in enumerate(flatten_selected_data):
+        data_point_folder = os.path.join(data_directory, str(index))
+        try:
+            os.mkdir(data_point_folder)
+            print(f"made data point {index}")
+        except FileExistsError:
+            os.rmdir(data_point_folder)
+            os.mkdir(data_point_folder)
+
+        for endings in file_endings:
+            shutil.copy(os.path.join(data, endings), data_point_folder)
+
 
 def main():
-    test_dataset_normality
+    pass
 
 
 if __name__ == '__main__':
