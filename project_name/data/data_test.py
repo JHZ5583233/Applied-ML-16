aafd6417a89f700bceadef99ec66ac0c38866dfd
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-12 12:38:47 +0200

    added normality test for data visualizer

--- project_name/data/data_test.py
+++ project_name/data/data_test.py
@@ -1,22 +1,27 @@
-import numpy as np
 import matplotlib.pyplot as plt
+from numpy import array, ndarray, load, concatenate
+from scipy.stats import normaltest
 
-file_end = "_depth.npy"
 
-
-def test_dataset_normality(data: list[str]) -> None:
+def test_dataset_normality(data: list[str], name: str) -> None:
     """
     plots the histogram of given depth maps.
 
     data: list of path names to the data point name
     """
-    whole_data = np.array([])
+    whole_data = array([])
     max_n = 0
 
     for data_point in data:
-        matrix: np.ndarray = np.load(data_point + file_end)
+        matrix: ndarray = load(data_point + "_depth.npy")
         max_n = max(max_n, matrix.max())
-        whole_data = np.concatenate([whole_data, matrix.flatten()])
+        whole_data = concatenate([whole_data, matrix.flatten()])
+
+    normality = normaltest(whole_data)
 
     plt.hist(whole_data)
+    plt.xlabel("Depth")
+    plt.ylabel("Frequency")
+    plt.title(f"{name} | norm: stat{normality.statistic}," +
+              "p:{normality.pvalue}")
     plt.show()
