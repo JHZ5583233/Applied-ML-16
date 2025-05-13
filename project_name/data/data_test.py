ba321d5cad33dfafe46045fd1eb3fcaf5aa3f143
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-14 00:27:23 +0200

    fixed the matplotlib bug and now it is always drawing the plot on the main thread

--- project_name/data/data_test.py
+++ project_name/data/data_test.py
@@ -3,6 +3,9 @@ from numpy import array, ndarray, load, concatenate
 from scipy.stats import normaltest
 
 
+multithread_data_test_output: list[tuple[str, ndarray]] = []
+
+
 def test_dataset_normality(data: list[str], name: str) -> None:
     """
     plots the histogram of given depth maps.
@@ -25,3 +28,20 @@ def test_dataset_normality(data: list[str], name: str) -> None:
     plt.title(f"{name} | norm: stat:{normality.statistic:0.2f}, " +
               f"p:{normality.pvalue:0.2f}")
     plt.show()
+
+
+def threaded_make_data_array(data: list[str], name: str) -> None:
+    """
+    create a tuple and append to thread output variable to plot on main thread.
+
+    data: list of path names to the data point name
+    """
+    whole_data = array([])
+    max_n = 0
+
+    for data_point in data:
+        matrix: ndarray = load(data_point + "_depth.npy")
+        max_n = max(max_n, matrix.max())
+        whole_data = concatenate([whole_data, matrix.flatten()])
+
+    multithread_data_test_output.append((name, whole_data))
--- project_name/data/subset_maker.py
+++ project_name/data/subset_maker.py
@@ -2,18 +2,16 @@ import os
 import random
 import shutil
 import threading
+import matplotlib.pyplot as plt
+from scipy.stats import normaltest
 from psutil import virtual_memory
-from data_test import test_dataset_normality
-from path_grapper import get_all_data_pathnames
-
-file_endings = {"rgb": ".png",
-                "depth": "_depth.npy",
-                "depth_mask": "_depth_mask.npy"}
+from data_test import test_dataset_normality, threaded_make_data_array, multithread_data_test_output
+from path_grapper import get_all_data_path_names
 
 
 def subset_full_dataset(amount_samples: int) -> None:
     """
-    This will sample n amount of samples from the datapoints in ful_data
+    This will sample n amount of samples from the data points in ful_data
     """
     # list all file endings.
     file_directory = __file__
@@ -21,32 +19,31 @@ def subset_full_dataset(amount_samples: int) -> None:
                                   "subset_data")
 
     print("getting data points")
-    all_datapoints_folder_pathnames = get_all_data_pathnames()
+    all_data_points_folder_path_names = get_all_data_path_names()
 
-    amount_differnt_main_folder = len(all_datapoints_folder_pathnames)
-    sample_per_main_folder = amount_samples // amount_differnt_main_folder
+    amount_full_data = (len(all_data_points_folder_path_names[0]) +
+                        len(all_data_points_folder_path_names[1]))
 
     print("sampling data points")
     selected_data: list[list[str]] = [
-        random.sample(main_folder, sample_per_main_folder) for
-        main_folder in all_datapoints_folder_pathnames]
+        random.sample(main_folder, int(amount_samples * (len(main_folder)/amount_full_data))) for
+        main_folder in all_data_points_folder_path_names]
 
     print("flattening data points")
-    flatten_selected_data = []
+    flatten_selected_data: list[str] = []
 
     for foldered_data in selected_data:
         flatten_selected_data += foldered_data
 
     print("running normality tests")
     # test normality.
-    if virtual_memory().total > float("inf"):
-        # TODO fix bug where matplot lib has to be on the main thread
+    if virtual_memory().total > 1600000000:
         threads: list[threading.Thread] = []
         for folder in selected_data:
-            threads.append(threading.Thread(target=test_dataset_normality,
+            threads.append(threading.Thread(target=threaded_make_data_array,
                                             args=(folder, "sub folder",)))
 
-        threads.append(threading.Thread(target=test_dataset_normality,
+        threads.append(threading.Thread(target=threaded_make_data_array,
                                         args=(flatten_selected_data,
                                               "whole data",)))
 
@@ -55,6 +52,18 @@ def subset_full_dataset(amount_samples: int) -> None:
 
         for thread in threads:
             thread.join()
+
+        for thread_output in multithread_data_test_output:
+            name, data = thread_output
+
+            normality = normaltest(data)
+
+            plt.hist(data)
+            plt.xlabel("Depth")
+            plt.ylabel("Frequency")
+            plt.title(f"{name} | norm: stat:{normality.statistic:0.2f}, " +
+                      f"p:{normality.pvalue:0.2f}")
+            plt.show()
     else:
         for folder in selected_data:
             test_dataset_normality(folder, "sub folder")
@@ -62,8 +71,8 @@ def subset_full_dataset(amount_samples: int) -> None:
         test_dataset_normality(flatten_selected_data, "whole selected data")
 
     print("copying over data to subset data")
-    # copy over the subsetted data in their own folders.
-    for index, data in enumerate(flatten_selected_data):
+    # copy over the subset data in their own folders.
+    for index, data_path in enumerate(flatten_selected_data):
         data_point_folder = os.path.join(data_directory, str(index))
         try:
             os.mkdir(data_point_folder)
@@ -72,15 +81,15 @@ def subset_full_dataset(amount_samples: int) -> None:
             os.rmdir(data_point_folder)
             os.mkdir(data_point_folder)
 
-        for endings in file_endings.values():
-            shutil.copy(data + endings, data_point_folder)
+        for endings in [".png", "_depth.npy", "_depth_mask.npy"]:
+            shutil.copy(data_path + endings, data_point_folder)
 
 
 def main() -> None:
     """
     This main to to run the data subset maker on it's own
     """
-    subset_full_dataset(500)
+    subset_full_dataset(1000)
 
 
 if __name__ == '__main__':
