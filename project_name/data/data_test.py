11620c2ce9dbaebf4b0e566aca73028953ceff6a
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-17 12:48:10 +0200

    only test depth under 100m

--- project_name/data/data_test.py
+++ project_name/data/data_test.py
@@ -21,6 +21,7 @@ def test_dataset_normality(data: list[str], name: str) -> None:
         max_n = max(max_n, matrix.max())
         whole_data = concatenate([whole_data, matrix.flatten()])
 
+    whole_data = whole_data[whole_data < 100]
     normality = normaltest(whole_data)
 
     plt.hist(whole_data)
@@ -44,5 +45,17 @@ def threaded_make_data_array(data: list[str], name: str) -> None:
         matrix: ndarray = load(data_point + "_depth.npy")
         max_n = max(max_n, matrix.max())
         whole_data = concatenate([whole_data, matrix.flatten()])
+    whole_data = whole_data[whole_data < 100]
 
     multithread_data_test_output.append((name, whole_data))
+
+
+def test_data(data, name):
+    normality = normaltest(data)
+
+    plt.hist(data)
+    plt.xlabel("Depth")
+    plt.ylabel("Frequency")
+    plt.title(f"{name} | norm: stat:{normality.statistic:0.2f}, " +
+              f"p:{normality.pvalue:0.2f}")
+    plt.show()
