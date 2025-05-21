61639049b81787747cd344f66c36c0f6dbc8eec0
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-21 21:50:58 +0200

    base layout made. need to fix bug of it rerunning itself over and over again. and implement save image

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
 
 
 def main():
@@ -13,6 +15,39 @@ def main():
     """
     st.markdown(intro_paragraph)
     st.divider()
+    rgb_image = None
+    depth_output = None
+    upload_text = """
+    Here you will upload an RGb 8-bit colour image for depth estimation.
+    """
+    st.markdown(upload_text)
+    uploaded_image = st.file_uploader(label="Upload image",
+                                      type=[".png", ".jpeg"])
+
+    if uploaded_image:
+        rgb_image = np.array(Image.open(uploaded_image))
+        image_size = rgb_image.shape
+        st.image(rgb_image)
+        st.write(f"height: {image_size[0]}, width: {image_size[1]}, " +
+                 f"channels: {image_size[2]}")
+
+        if image_size[2] > 3:
+            st.error("We do not accept RGBa images.")
+
+    run_model = st.button("Start conversion.")
+
+    if run_model and rgb_image.any():
+        st.write("work in progress")
+
+        depth_output = np.random.random((image_size[0], image_size[1], 1))
+
+    if depth_output is not None and depth_output.any():
+        st.image(depth_output)
+
+    prepare_export = st.button("prepare depth map export")
+
+    if prepare_export and depth_output.any():
+        pass
 
 
 if __name__ == '__main__':
