e23b751671ffb9a2676a64b7c672f9e026647e1b
JHZ5583233 <j.h.zhang@student.rug.nl>
2025-05-27 14:12:02 +0200

    added model loading

--- streamlit_main.py
+++ streamlit_main.py
@@ -2,12 +2,29 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from io import BytesIO
+from project_name.models.cnn import CNNBackbone
+from os.path import split, join
+import torch
 
 
 def main() -> None:
     """
     Main function where the stream lit app runs.
     """
+    if not ("device" in st.session_state):
+        st.session_state["device"] = torch.device("cuda"
+                                                  if torch.cuda.is_available()
+                                                  else "cpu")
+
+    if not ("model" in st.session_state):
+        file_location = split(__file__)[0]
+        st.session_state["model"] = CNNBackbone(pretrained=True).to(
+            st.session_state["device"]
+        )
+        st.session_state["model"].load_state_dict(torch.load(
+            join(file_location, "cnn_best.pth"), weights_only=True))
+        st.session_state["model"].eval()
+
     st.title("Streamlit demo for Applied Machine Learning: Depth prediction.")
     st.divider()
     intro_paragraph = """
@@ -33,6 +50,7 @@ def main() -> None:
         st.image(rgb_image)
         st.write(f"height: {image_size[0]}, width: {image_size[1]}, " +
                  f"channels: {image_size[2]}")
+        st.session_state["rgb_image"] = rgb_image
 
         if image_size[2] > 3:
             st.error("We do not accept RGBa images.")
@@ -46,14 +64,23 @@ def main() -> None:
 
     run_model = st.button("Start conversion.")
 
-    if run_model and rgb_image.any():
+    if run_model and "rgb_image" in st.session_state:
         st.write("work in progress")
+        model = st.session_state["model"]
+        input_image = st.session_state["rgb_image"]
+        orginal_shape = list(input_image.shape)[0:2] + [1]
+        input_image = np.expand_dims(input_image, axis=0)
 
-        st.session_state["depth_output"] = np.random.random((image_size[0],
-                                                             image_size[1], 1))
+        st.session_state["depth_output"] = model(
+            torch.tensor(input_image,
+                         device=st.session_state["device"],
+                         dtype=torch.float).permute(0, 3, 1, 2)
+            ).detach().numpy().reshape(orginal_shape)
 
     if "depth_output" in st.session_state:
-        st.image(st.session_state["depth_output"])
+        max = np.max(st.session_state["depth_output"])
+        st.write((st.session_state["depth_output"].squeeze() / max) * 255)
+        st.image((st.session_state["depth_output"] / max) * 255, clamp=True)
         depth_image_size = st.session_state["depth_output"].shape
         st.write(f"height: {depth_image_size[0]}," +
                  f" width: {depth_image_size[1]}, " +
